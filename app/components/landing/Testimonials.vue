a38c668021106f56d63f8dc24142ca905b96c62f
zerootem <zerootemt@gmail.com>
2026-07-11 20:24:11 +0300

    Update Testimonials.vue
    تجاوب اللغه

--- app/components/landing/Testimonials.vue
+++ app/components/landing/Testimonials.vue
@@ -28,7 +28,7 @@ defineProps<{
         class="rounded-none"
         :ui="{
           container: 'sm:py-12 lg:py-12 sm:gap-8',
-          description: 'text-base! text-balance before:content-[open-quote] before:text-5xl lg:before:text-7xl before:inline-block before:text-dimmed before:absolute before:-ml-6 lg:before:-ml-10 before:-mt-2 lg:before:-mt-4 after:content-[close-quote] after:text-5xl lg:after:text-7xl after:inline-block after:text-dimmed after:absolute after:mt-1 lg:after:mt-0 after:ml-1 lg:after:ml-2'
+          description: 'text-base! text-balance text-start before:content-[open-quote] before:text-5xl lg:before:text-7xl before:inline-block before:text-dimmed before:absolute before:-ml-6 rtl:before:-mr-6 lg:before:-ml-10 rtl:lg:before:-mr-10 before:-mt-2 lg:before:-mt-4 after:content-[close-quote] after:text-5xl lg:after:text-7xl after:inline-block after:text-dimmed after:absolute after:mt-1 lg:after:mt-0 after:ml-1 rtl:after:mr-1 lg:after:ml-2 rtl:lg:after:mr-2'
         }"
       >
         <UUser
